666bfefeda2982f1448fa148f36d1f80612ca331
SumanSingha05 <sumansingha8654@gmail.com>
2024-07-24 19:39:37 +0530

    Changes Done

---
+++ Blog/src/appwrite/auth.js
@@ -0,0 +1,11 @@
+import conf from '../conf.js'
+
+import { Client , Account , ID} from "appwrite"
+
+
+export class Authservice {}
+
+const authService = new AuthService();
+
+
+export default authService
