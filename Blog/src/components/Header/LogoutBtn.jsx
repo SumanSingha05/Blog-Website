2bb5468668c2196683c679528876083d3ce91ad6
SumanSingha05 <sumansingha8654@gmail.com>
2024-07-25 10:21:02 +0530

    Logout Option Initialization Done

---
+++ Blog/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import authService from '../../appwrite/config'
+import { logout } from '../../store/authSlice'
+
+function LogoutBtn () {
+    const dispatch =  useDispatch
+  return (
+    <div>LogoutBtn</div>
+  )
+}
+
+export default LogoutBtn
