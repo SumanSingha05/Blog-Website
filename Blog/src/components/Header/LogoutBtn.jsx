ea1783c2c5eddd1195c4b408377b579556f33fa4
SumanSingha05 <sumansingha8654@gmail.com>
2024-07-25 23:53:07 +0530

    Header , Logout Config Done

--- Blog/src/components/Header/LogoutBtn.jsx
+++ Blog/src/components/Header/LogoutBtn.jsx
@@ -4,9 +4,18 @@ import authService from '../../appwrite/config'
 import { logout } from '../../store/authSlice'
 
 function LogoutBtn () {
-    const dispatch =  useDispatch
+    const dispatch =  useDispatch()
+      const handleHandler = () => {
+        authService.logout().then(() => {
+          dispatch(logout())
+        })
+      }
+
   return (
-    <div>LogoutBtn</div>
+    <button
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}
+    >Logout</button>
   )
 }
 
